dc5e7af02cd5fb103568d140201c34b5f1211aad
Max Elia <max_elia@hotmail.de>
2021-10-14 03:43:15 +0200

    Add 'next' theia version to nightly build
    Signed-off-by: Max Elia Schweigkofler <max_elia@hotmail.de>

--- .github/workflows/generate-and-build-all-templates.sh
+++ .github/workflows/generate-and-build-all-templates.sh
@@ -2,13 +2,24 @@ npm install -g yo
 npm link
 mkdir tmp
 cd tmp
-exit_code=0
+theia_v="$1"
+failed_templates=()
 for d in ../templates/*/ ; do
+    exit_code=0
     extension=$(basename $d)
     mkdir $extension
     cd $extension
-    yo theia-extension $extension -y $extension || exit_code=$?
+    yo theia-extension $extension -y $extension -t $theia_v || exit_code=$?
     cd ..
+    rm -rf $extension
+    if [ $exit_code -ne 0 ]; then
+        failed_templates+=( "$extension failed with Code: $exit_code" )
+    fi
 done
 cd ..
-exit $exit_code
+if [ ${#failed_templates[@]} -ne 0 ]; then
+    printf '%s\n' "${failed_templates[@]}"
+    exit 1
+else 
+    exit 0
+fi
