0a0d3ec949ec97de7af9d3133fb6d149d0d8d9c6
counter2015 <hujiawei7@outlook.com>
2025-02-17 06:59:44 +0800

    Fix depreated imports. (#239)
    Signed-off-by: counter2015 <voidcounter@gmail.com>

--- templates/backend/backend-module.ts
+++ templates/backend/backend-module.ts
@@ -1,4 +1,4 @@
-import { ConnectionHandler, JsonRpcConnectionHandler } from '@theia/core';
+import { ConnectionHandler, RpcConnectionHandler } from '@theia/core';
 import { ContainerModule } from '@theia/core/shared/inversify';
 import { BackendClient, HelloBackendWithClientService, HelloBackendService, HELLO_BACKEND_PATH, HELLO_BACKEND_WITH_CLIENT_PATH } from '../common/protocol';
 import { HelloBackendWithClientServiceImpl } from './hello-backend-with-client-service';
@@ -7,14 +7,14 @@ import { HelloBackendServiceImpl } from './hello-backend-service';
 export default new ContainerModule(bind => {
     bind(HelloBackendService).to(HelloBackendServiceImpl).inSingletonScope()
     bind(ConnectionHandler).toDynamicValue(ctx =>
-        new JsonRpcConnectionHandler(HELLO_BACKEND_PATH, () => {
+        new RpcConnectionHandler(HELLO_BACKEND_PATH, () => {
             return ctx.container.get<HelloBackendService>(HelloBackendService);
         })
     ).inSingletonScope();
 
     bind(HelloBackendWithClientService).to(HelloBackendWithClientServiceImpl).inSingletonScope()
     bind(ConnectionHandler).toDynamicValue(ctx =>
-        new JsonRpcConnectionHandler<BackendClient>(HELLO_BACKEND_WITH_CLIENT_PATH, client => {
+        new RpcConnectionHandler<BackendClient>(HELLO_BACKEND_WITH_CLIENT_PATH, client => {
             const server = ctx.container.get<HelloBackendWithClientServiceImpl>(HelloBackendWithClientService);
             server.setClient(client);
             client.onDidCloseConnection(() => server.dispose());
